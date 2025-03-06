ef69283fd55ef09f51106f19beda888f03948ea2
CristinaT21 <cristarna21@gmail.com>
2025-03-06 23:16:23 +0200

    Scale model

--- Lesson7(C5_w3-4)/v2.py
+++ Lesson7(C5_w3-4)/v2.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32
-block_size = 8 
+batch_size = 64
+block_size = 256 
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
 torch.manual_seed(1337)
 
@@ -64,6 +67,8 @@ class Head(nn.Module):
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
         
+        self.dropout = nn.Dropout(dropout)
+        
     def forward(self, x):
         B, T, C = x.shape
         k = self.key(x)
@@ -72,6 +77,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-1, -2) / (C ** -0.5)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
         wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
         # apply the attention scores to the values
         v = self.value(x)
         out = wei @ v
@@ -84,10 +90,12 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
         
     def forward(self, x):
         out = torch.cat([head(x) for head in self.heads], dim=-1)
-        return self.proj(out)
+        out = self.dropout(self.proj(out))
+        return out
 
 class FeedForward(nn.Module):
     """ simple linear layer followed by ReLU """
@@ -98,6 +106,7 @@ class FeedForward(nn.Module):
             nn.Linear(n_embd, 4*n_embd),
             nn.ReLU(),
             nn.Linear(4*n_embd, n_embd),
+            nn.Dropout(dropout),
         )
         
     def forward(self, x):
@@ -130,12 +139,8 @@ class BiagramLanguageModel(nn.Module):
         # self.sa_head = Head(n_embd)
         # self.sa_heads = MultiHeadAttention(num_heads=4, head_size=n_embd//4)
         # self.ffwd = FeedForward(n_embd)
-        self.blocks = nn.Sequential(
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),  
-            nn.LayerNorm(n_embd),  
-        )
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
         
     def forward(self, idx, targets=None):
@@ -148,6 +153,7 @@ class BiagramLanguageModel(nn.Module):
         # x = self.sa_heads(x)
         # x = self.ffwd(x)
         x = self.blocks(x)
+        x = self.ln_f(x)
         logits = self.lm_head(x)  # B, T, vocab_size
         
         if targets is None:
